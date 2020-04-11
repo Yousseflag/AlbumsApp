b2f5a0b4f98e59479ed818510b006399e8032ecf
yousseflagrouh <youssef.lagrouh1@gmail.com>
2020-04-11 19:13:50 +0200

    feat: project init

---
+++ app/src/main/java/lbc/testech/albumsapp/AlbumListActivity.kt
@@ -0,0 +1,12 @@
+package lbc.testech.albumsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlbumListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_album_list)
+    }
+}
