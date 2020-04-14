87b310459cb2d1d6de76b78f25c1baad0bc7bfca
Yousseflag <youssef.lagrouh1@gmail.com>
2020-04-14 12:05:50 +0200

    Revert "Add api components"

--- api/src/main/java/di/ApiModule.kt
+++
@@ -1,9 +0,0 @@
-package di
-
-import api.AlbumsApi
-import org.koin.dsl.module
-
-val apiModule = module {
-
-    single { ServiceGenerator.createService(AlbumsApi.API_URL, AlbumsApi::class.java) }
-}
